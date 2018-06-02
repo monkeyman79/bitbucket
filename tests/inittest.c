fe744375baeb637f6777de878b478d8e29293266
monkeyman79 <tkijkowski@gmail.com>
2018-06-02 02:12:50 +0200

    Tests with dlopen functions

--- tests/inittest.c
+++ tests/inittest.c
@@ -3,6 +3,8 @@
 #include <getopt.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdint.h>
+#include <dlfcn.h>
 
 int cont_error = 0;
 int twice = 0;
@@ -11,28 +13,39 @@ int no_quit = 0;
 int segv = 0;
 int ignore_pipe = 0;
 int handle_pipe = 0;
+int handle_segv = 0;
 int handle_first = 0;
+int unloaded = 0;
 int wait = 0;
 
 int* null_ptr = 0;
+int (*pSDL_Init)(uint32_t flags);
+void (*pSDL_Quit)(void);
+const char* (*pSDL_GetError)(void);
+
+#ifdef DYNAMIC_LOAD
+void *libsdl2_handle;
+#endif
 
 struct option long_options[] = {
     {"continue",       no_argument, 0, 'c'},
     {"twice",          no_argument, 0, 't'},
     {"no-init",        no_argument, 0, 'i'},
     {"no-quit",        no_argument, 0, 'q'},
-    {"raise-segv",     no_argument, 0, 's'},
+    {"raise-sigsegv",  no_argument, 0, 's'},
     {"ignore-sigpipe", no_argument, 0, 'p'},
     {"handle-sigpipe", no_argument, 0, 'a'},
+    {"handle-sigsegv", no_argument, 0, 'e'},
     {"handler-first",  no_argument, 0, 'f'},
     {"wait",           no_argument, 0, 'w'},
+    {"unloaded",       no_argument, 0, 'u'},
     {"help",           no_argument, 0, 'h'},
     {0, 0, 0, 0}
 };
 
-void check_error(int error)
+void check_error(int error, const char* (*geterror)(void))
 {
-    fprintf(stderr, "%s\n", error ? SDL_GetError(): "OK");
+    fprintf(stderr, "%s\n", error ? geterror(): "OK");
     if (error) {
         if (!cont_error) {
             fprintf(stderr, "...exiting\n");
@@ -49,7 +62,16 @@ void set_ignore_pipe()
 
 void pipe_handler(int sig)
 {
-    fprintf(stderr, "SIGPIPE raised\n");
+    fprintf(stderr, "inittest SIGPIPE handler executing\n");
+    return;
+}
+
+void segv_handler(int sig)
+{
+    sigset_t sigset;
+    fprintf(stderr, "inittest SIGSEGV handler executing\n");
+    fprintf(stderr, "inittest SIGSEGV handler re-raising\n");
+    raise(SIGSEGV);
     return;
 }
 
@@ -58,7 +80,17 @@ void set_handle_pipe()
     struct sigaction sa = { 0 };
     sa.sa_handler = &pipe_handler;
     sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
     sigaction(SIGPIPE, &sa, NULL);
+}
+
+void set_handle_segv()
+{
+    struct sigaction sa = { 0 };
+    sa.sa_handler = &segv_handler;
+    sa.sa_flags = SA_NODEFER | SA_RESETHAND;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGSEGV, &sa, NULL);
 }
 
 void usage(const char* argv0)
@@ -70,12 +102,90 @@ void usage(const char* argv0)
 "  -t, --twice           Call SDL_Init and SDL_Quit twice\n"
 "  -i, --no-init         Skip SDL_Init\n"
 "  -q, --no-quit         Skip SDL_Quit\n"
-"  -s, --raise-segv      Raise fatal signal by NULL pointer access\n"
 "  -p, --ignore-sigpipe  Set SIGPIPE handler to ignore\n"
-"  -a, --handle-sigpipe  Install SIGPIPE handler showing message\n"
-"  -f, --handler-first   Set SIGPIPE handler before SDL_Init\n"
-"  -w, --wait            Add 20 seconds delay before writing to stdout\n",
+"  -a, --handle-sigpipe  Install SIGPIPE handler\n"
+"  -e, --handle-sigsegv  Install SIGSEGV handler\n"
+"  -f, --handler-first   Install signal handlers before SDL_Init\n"
+"  -s, --raise-sigsegv   Raise fatal signal by NULL pointer access\n"
+"  -w, --wait            Add 20 seconds delay before signal or writing to stdout\n"
+"  -u, --unloaded        Raise signal or print message after SDL_Quit\n",
         argv0);
+}
+
+#ifndef DYNAMIC_LOAD
+#define cSDL_Init SDL_Init
+#define cSDL_Quit SDL_Quit
+#define cSDL_GetError SDL_GetError
+#else
+#define cSDL_Init pSDL_Init
+#define cSDL_Quit pSDL_Quit
+#define cSDL_GetError pSDL_GetError
+#endif
+
+#define _dlerror ((const char* (*)(void))dlerror)
+
+int init_sdl()
+{
+#ifdef DYNAMIC_LOAD
+    fprintf(stderr, "dlopen(libSDL2.so): ");
+    // libsdl2_handle = dlopen("libSDL2.so", RTLD_LAZY);
+    libsdl2_handle = dlopen("/usr/local/lib/libSDL2.so", RTLD_LAZY);
+    check_error(libsdl2_handle == NULL, _dlerror);
+
+    fprintf(stderr, "dlsym(SDL_Init): ");
+    pSDL_Init = (int (*)(uint32_t)) dlsym(libsdl2_handle, "SDL_Init");
+    check_error(pSDL_Init == NULL, _dlerror);
+
+    fprintf(stderr, "dlsym(SDL_Quit): ");
+    pSDL_Quit = (void (*)(void)) dlsym(libsdl2_handle, "SDL_Quit");
+    check_error(pSDL_Quit == NULL, _dlerror);
+
+    fprintf(stderr, "dlsym(SDL_GetError): ");
+    pSDL_GetError = (const char* (*)(void)) dlsym(libsdl2_handle, "SDL_GetError");
+    check_error(pSDL_GetError == NULL, _dlerror);
+#endif
+
+    fprintf(stderr, "SDL_Init: ");
+    if (!no_init)
+        check_error(cSDL_Init(SDL_INIT_VIDEO) != 0, cSDL_GetError);
+    else
+        fprintf(stderr, "Skipped\n");
+}
+
+int quit_sdl()
+{
+    fprintf(stderr, "SDL_Quit: ");
+    if (!no_quit)
+    {
+        cSDL_Quit();
+        fprintf(stderr, "Done\n");
+    }
+    else
+        fprintf(stderr, "Skipped\n");
+
+#ifdef DYNAMIC_LOAD
+    fprintf(stderr, "dlclose(libSDL2.so): ");
+    check_error(dlclose(libsdl2_handle) != 0, _dlerror);
+#endif
+}
+
+void install_sig_handlers()
+{
+    if (ignore_pipe) {
+        fprintf(stderr, "Ignore SIGPIPE: ");
+        set_ignore_pipe();
+        fprintf(stderr, "Done\n");
+    }
+    else if (handle_pipe) {
+        fprintf(stderr, "Set SIGPIPE handler: ");
+        set_handle_pipe();
+        fprintf(stderr, "Done\n");
+    }
+    if (handle_segv) {
+        fprintf(stderr, "Set SIGSEGV handler: ");
+        set_handle_segv();
+        fprintf(stderr, "Done\n");
+    }
 }
 
 int main(int argc, char** argv)
@@ -83,7 +193,7 @@ int main(int argc, char** argv)
     int c;
     while (1) {
         int option_index = 0;
-        c = getopt_long(argc, argv, "ctiqspafwh", long_options,
+        c = getopt_long(argc, argv, "ctiqspaefwuh", long_options,
                 &option_index);
 
         if (c == -1)
@@ -104,9 +214,13 @@ int main(int argc, char** argv)
 
             case 'a': handle_pipe = 1; break;
 
+            case 'e': handle_segv = 1; break;
+
             case 'f': handle_first = 1; break;
 
             case 'w': wait = 1; break;
+
+            case 'u': unloaded = 1; break;
 
             case '?': return 1;
 
@@ -129,52 +243,23 @@ int main(int argc, char** argv)
     }
 
     if (handle_first) {
-        if (ignore_pipe) {
-            fprintf(stderr, "Ignore SIGPIPE: ");
-            set_ignore_pipe();
-            fprintf(stderr, "Done\n");
-        }
-        else if (handle_pipe) {
-            fprintf(stderr, "Set SIGPIPE handler: ");
-            set_handle_pipe();
-            fprintf(stderr, "Done\n");
-        }
+        install_sig_handlers();
     }
 
     if (twice) {
-        fprintf(stderr, "SDL_Init: ");
-        if (!no_init)
-            check_error(SDL_Init(SDL_INIT_VIDEO) != 0);
-        else
-            fprintf(stderr, "Skipped\n");
+        init_sdl();
 
-        fprintf(stderr, "SDL_Quit: ");
-        if (!no_quit)
-        {
-            SDL_Quit();
-            fprintf(stderr, "Done\n");
-        }
-        else
-            fprintf(stderr, "Skipped\n");
+        quit_sdl();
     }
 
-    fprintf(stderr, "SDL_Init: ");
-    if (!no_init)
-        check_error(SDL_Init(SDL_INIT_VIDEO) != 0);
-    else
-        fprintf(stderr, "Skipped\n");
+    init_sdl();
 
     if (!handle_first) {
-        if (ignore_pipe) {
-            fprintf(stderr, "Ignore SIGPIPE: ");
-            set_ignore_pipe();
-            fprintf(stderr, "Done\n");
-        }
-        else if (handle_pipe) {
-            fprintf(stderr, "Set SIGPIPE handler: ");
-            set_handle_pipe();
-            fprintf(stderr, "Done\n");
-        }
+        install_sig_handlers();
+    }
+
+    if (unloaded) {
+        quit_sdl();
     }
 
     if (segv) {
@@ -189,16 +274,14 @@ int main(int argc, char** argv)
         fprintf(stderr, "Done\n");
     }
 
-    printf("stdout rollo\n");
+    printf("this goes to stdout\n");
     fflush(stdout);
 
-    fprintf(stderr, "SDL_Quit: ");
-    if (!no_quit)
-    {
-        SDL_Quit();
-        fprintf(stderr, "Done\n");
+    if (!unloaded) {
+        quit_sdl();
     }
-    else
-        fprintf(stderr, "Skipped\n");
+
+    fprintf(stderr, "Bye!\n");
 }
 
+/* vi: set ts=4 sw=4 expandtab: */
